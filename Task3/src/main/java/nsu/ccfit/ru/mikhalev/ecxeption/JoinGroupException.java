82d98cda0728e4f7e8fbafc11c0ef6bb09180e06
MikhalevMakar <mikhalevmakar@mail.ru>
2023-10-18 18:13:11 +0700

    delete task3

--- Task3/src/main/java/nsu/ccfit/ru/mikhalev/ecxeption/JoinGroupException.java
+++
@@ -1,7 +0,0 @@
-package nsu.ccfit.ru.mikhalev.ecxeption;
-
-public class JoinGroupException extends RuntimeException {
-    public JoinGroupException(String ip, int port) {
-        super(String.format("Exception join to group by ip %s port %d: ", ip, port));
-    }
-}
--- Task3/src/main/java/nsu/ccfit/ru/mikhalev/ecxeption/SwitchTailException.java
+++
@@ -1,7 +0,0 @@
-package nsu.ccfit.ru.mikhalev.ecxeption;
-
-public class SwitchTailException extends RuntimeException {
-    public SwitchTailException() {
-        super("failed to find a position for the tail");
-    }
-}
